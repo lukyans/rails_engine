42b435799418afae5e78d4ce6fcf5a958459d31b
lukyans <sukyans@gmail.com>
2017-05-02 15:35:51 -0600

    create customers table

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :customer do
     fist_name "MyString"
     last_name "MyString"
-    created_at "2017-05-02 15:26:17"
-    updated_at "2017-05-02 15:26:17"
+    created_at "2017-05-02 15:34:08"
+    updated_at "2017-05-02 15:34:08"
   end
 end
