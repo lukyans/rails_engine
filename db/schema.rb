d1599d0bb24d4d30540767dcbf6a52e690137f71
NessEFC <NessEFC@gmail.com>
2017-05-02 15:58:24 -0600

    create and run migration to create items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502001151) do
+ActiveRecord::Schema.define(version: 20170502215515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.text     "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.text     "name"
---
+++ spec/factories/items.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :item do
+    name "MyText"
+    description "MyText"
+    unit_price 1
+    created_at "2017-05-02 15:55:15"
+    updated_at "2017-05-02 15:55:15"
+  end
+end
