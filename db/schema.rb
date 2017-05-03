8fff32b0ce42f2d0c2c661cd400e73dee359ee16
lukyans <sukyans@gmail.com>
2017-05-03 11:15:31 -0600

    add invoice column to invoice_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
-ActiveRecord::Schema.define(version: 20170503154323) do
-
+ActiveRecord::Schema.define(version: 20170503170949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,14 +22,16 @@ ActiveRecord::Schema.define(version: 20170503154323) do
     t.datetime "updated_at"
   end
 
-
   create_table "invoice_items", force: :cascade do |t|
     t.integer  "item_id"
     t.integer  "quantity"
     t.integer  "unit_price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "invoice_id"
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id", using: :btree
     t.index ["item_id"], name: "index_invoice_items_on_item_id", using: :btree
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.string   "status"
@@ -43,7 +41,6 @@ ActiveRecord::Schema.define(version: 20170503154323) do
     t.integer  "merchant_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id", using: :btree
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id", using: :btree
-
   end
 
   create_table "items", force: :cascade do |t|
@@ -70,11 +67,9 @@ ActiveRecord::Schema.define(version: 20170503154323) do
     t.datetime "updated_at"
   end
 
-
+  add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
-
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
-
   add_foreign_key "items", "merchants"
 end
