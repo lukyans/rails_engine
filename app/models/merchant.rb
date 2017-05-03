2a14dd2c986c5044c9291895438870e23d8df4db
NessEFC <NessEFC@gmail.com>
2017-05-03 17:34:40 -0600

    add association from merchant to transaction

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,5 +3,6 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+  has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,5 @@ class Transaction < ApplicationRecord
 
   belongs_to :invoice
   has_one :customer, through: :invoice
+  has_one :merchant, through: :invoice
 end
