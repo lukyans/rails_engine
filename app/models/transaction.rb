0437ebd5e482abc0595971406a6c4fb91e081df2
NessEFC <NessEFC@gmail.com>
2017-05-03 20:37:31 -0600

    add associations to models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,4 +6,6 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
   has_one :customer, through: :invoice
   has_one :merchant, through: :invoice
+
+  scope :successful, -> { where result: "success" }
 end
