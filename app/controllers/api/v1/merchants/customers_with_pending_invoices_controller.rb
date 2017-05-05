2859cc6ce5d7c5d4cf3676fc88c9d42636148c9a
lukyans <sukyans@gmail.com>
2017-05-05 00:24:53 -0600

    add boss mode

---
+++ app/controllers/api/v1/merchants/customers_with_pending_invoices_controller.rb
@@ -0,0 +1,11 @@
+# module Api
+#   module V1
+#     module Merchants
+#       class CustomersWithPendingInvoicesController < ApplicationController
+#         def show
+#           render json: Merchant.customers_with_pending_invoices(params[:merchant_id])
+#         end
+#       end
+#     end
+#   end
+# end
